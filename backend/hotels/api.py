289525d0327e28de354b810cb19e36a82febe58d
Rajiv Ranjan Kumar <rrks.195@gmail.com>
2025-11-16 01:20:00 +0530

    16-11-2025 | work

--- backend/hotels/api.py
+++ backend/hotels/api.py
@@ -1,10 +1,11 @@
 from typing import Optional
-from ninja import Router
+from ninja import Query, Router
+from ninja.pagination import paginate, PageNumberPagination
 
 from accounts.models import UserGallery
-from hotels.queries import add_new_hotel, get_hotel_by_id
+from hotels.queries import add_new_hotel, delete_hotel_by_id, filter_hotel, get_hotel_by_id, update_hotel_by_id
 from utils.helper import authorize_request, secure, validate_model_id
-from hotels.schema import HotelSchemaIn, HotelSchemaOut
+from hotels.schema import HotelSchemaFilter, HotelSchemaIn, HotelSchemaOut
 
 
 
@@ -26,8 +27,43 @@ async def register_hotel(request, payload: HotelSchemaIn, user_id: Optional[int]
 
 
 
+@router.put("/hotel/{hotel_id}", response=HotelSchemaOut, auth=secure())
+@authorize_request
+async def update_hotel(request, hotel_id: int, payload: HotelSchemaIn, user_id: Optional[int] = None):
+    await validate_model_id(value=payload.logo, model=UserGallery, field_name="Logo")
+    hotel_obj = await update_hotel_by_id(hotel_id=hotel_id, payload=payload, user_id=user_id)
+    return hotel_obj
+
+
+
+
+
+
 @router.get("/hotel/{hotel_id}", response=HotelSchemaOut, auth=secure())
 @authorize_request
 async def get_hotel(request, hotel_id: int, user_id: Optional[int] = None):
     hotel_obj = await get_hotel_by_id(hotel_id=hotel_id, user_id=user_id)
     return hotel_obj
+
+
+
+
+
+@router.get("/hotel", response=list[HotelSchemaOut], auth=secure())
+@authorize_request
+@paginate(PageNumberPagination, page_size=10)
+async def filter_hotels(request, filters: HotelSchemaFilter = Query(...), user_id: Optional[int] = None):
+    hotels = await filter_hotel(filters=filters, user_id=user_id)
+    return hotels
+
+
+
+
+
+
+
+@router.delete("/hotel/{hotel_id}", auth=secure())
+@authorize_request
+async def delete_hotel(request, hotel_id: int, user_id: Optional[int] = None):
+    hotel_obj = await delete_hotel_by_id(hotel_id=hotel_id, user_id=user_id)
+    return {"detail": "Hotel deleted successfully."}
--- backend/hotels/schema.py
+++ backend/hotels/schema.py
@@ -25,9 +25,68 @@ class HotelSchemaIn(ModelSchema):
 
 
 
+class HotelSchemaFilter(ModelSchema):
+    name: Optional[str] = None
+
+    class Meta:
+        model = Hotel
+        fields = ["name"]
+    
+    @model_validator(mode="after")
+    def validate_all(self):
+        validate_string(value=self.name, field_name="Hotel Name")
+        
+        return self
+
+
+
+
+
+
+
+
+
 class HotelSchemaOut(ModelSchema):
     logo: Optional[UserGallerySchemaOut] = None
 
     class Meta:
         model = Hotel
         fields = ["id", "name", "description", "logo", "created_at"]
+
+
+
+
+
+
+
+class BranchSchemaIn(ModelSchema):
+    class Meta:
+        model = Hotel
+        exclude = ["id","hotel","is_active","created_at"]
+        
+    @model_validator(mode="after")
+    def validate_all(self):
+        validate_string(value=self.name, field_name="Branch Name", is_required=True)
+        validate_string(value=self.address, field_name="Address", is_required=True)
+        validate_string(value=self.country, field_name="Country", is_required=True)
+        validate_string(value=self.state, field_name="State", is_required=True)
+        validate_string(value=self.city, field_name="City", is_required=True)
+        validate_email(value=self.pincode, field_name="Pincode", is_required=True)
+        validate_number(value=self.latitude, field_name="Latitude", is_required=False)
+        validate_number(value=self.longitude, field_name="Longitude", is_required=False)
+        
+        return self
+
+
+
+
+
+
+
+
+
+
+class BranchSchemaOut(ModelSchema):
+    class Meta:
+        model = Hotel
+        fields = "__all__"
